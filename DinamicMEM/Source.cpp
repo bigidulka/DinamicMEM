b7f372606a3e5e5266a7970750ddf371a01958d5
Bogdan <udincevb@gmail.com>
2021-03-12 20:50:01 +0300

    fillrand templated

--- DinamicMEM/Source.cpp
+++ DinamicMEM/Source.cpp
@@ -159,8 +159,9 @@ void Erase(int*& arr, int& n, int index)
 
 template<typename T> T** Allocate(const int m, const int n);
 template<typename T> void Clear(T** arr, const int m);
-void FillRand(int* arr, const int n);
-void FillRand(int** arr, const int m, const int n);
+//template<typename T> void Random(T& value);
+template<typename T> void FillRand(T* arr, const int m, const int n);
+template<typename T> void FillRand(T** arr, const int m, const int n);
 template<typename T> void Print(T* arr, const int n);
 template<typename T> void Print(T** arr, const int m, const int n);
 
@@ -237,14 +238,15 @@ void main()
 	Clear(arr, m);
 }
 
-void FillRand(int* arr, const int m, const int n)
+template<typename T> void FillRand(T* arr, const int m, const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
 		*(arr + i) = rand() % 100;
+		//Random(arr[i]);
 	}
 }
-void FillRand(int** arr, const int m, const int n)
+template<typename T> void FillRand(T** arr, const int m, const int n)
 {
 	for (int i = 0; i < m; i++)
 	{
@@ -252,6 +254,7 @@ void FillRand(int** arr, const int m, const int n)
 		for (int j = 0; j < n; j++)
 		{
 			arr[i][j] = rand() % 100;
+			//Random(arr[i]);
 		}
 	}
 }
@@ -291,6 +294,17 @@ template<typename T> void Clear(T** arr, const int m)
 	}
 	delete[] arr;
 }
+
+//template<typename T>void Random(T& value)
+//{
+//	//cout << typeid(value).name() << endl;
+//	if (typeid(value) == typeid(float) || typeid(value) == typeid(double))
+//		value = double(rand() % 10000) / 100;
+//	else if (typeid(value) == typeid(char))
+//		value = rand();
+//	else
+//		value = rand() % 100;
+//}
 
 template<typename T> T** push_row_back(T** arr, int& m, const int n)
 {
